33835e2f32a94fb8c32fe306b5aa89b964da624d
Sergey Bochkov <bochkovsa@gmail.com>
2015-12-21 00:40:07 +0500

    убраны концеренция Андронова и сайт it-students

--- src/main/java/com/sergeybochkov/rss/Application.java
+++ src/main/java/com/sergeybochkov/rss/Application.java
@@ -2,10 +2,18 @@ package com.sergeybochkov.rss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@ComponentScan(excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.REGEX, pattern = {
+                "com.sergeybochkov.rss.andronov.*",
+                "com.sergeybochkov.rss.itstudent.*",
+        })
+})
 public class Application {
 
     public static void main(String[] args) {
