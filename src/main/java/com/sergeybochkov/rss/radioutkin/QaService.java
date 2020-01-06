b0d6efea95fa28132148968d019d27050168d99e
Sergey Bochkov <bochkovsa@gmail.com>
2020-01-06 11:23:39 +0500

    deleted radioutkin

--- src/main/java/com/sergeybochkov/rss/radioutkin/QaService.java
+++
@@ -1,8 +0,0 @@
-package com.sergeybochkov.rss.radioutkin;
-
-import java.util.List;
-
-public interface QaService {
-
-    List<Qa> getLatest(int limit);
-}
--- src/main/java/com/sergeybochkov/rss/radioutkin/source/Source.java
+++
@@ -1,8 +0,0 @@
-package com.sergeybochkov.rss.radioutkin.source;
-
-import java.io.IOException;
-
-public interface Source {
-
-    void download() throws IOException;
-}
